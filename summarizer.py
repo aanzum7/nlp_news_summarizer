688e82262a208b8d57f974747f6adbcb95d6b53a
Tanvir Anzum <50626067+aanzum7@users.noreply.github.com>
2026-07-04 23:37:37 +0600

    Fix typo in page title of summarizer.py

--- summarizer.py
+++ summarizer.py
@@ -10,7 +10,7 @@ import re
 # ---------------------------
 # ✅ Page Config (Must be First)
 # ---------------------------
-st.set_page_config(page_title="InsightInMinutes | 1Minute AI News Reader", page_icon="🔎", layout="wide")
+st.set_page_config(page_title="InsightInMinutes | 1Minute AI News Readerr", page_icon="🔎", layout="wide")
 
 # Initialize Session States
 if "last_summary" not in st.session_state:
